8b00709f0506ed013283ad13cc2ea779659c715b
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2026-08-29 13:28:15 +0530

    init commit

---
+++ src/main/java/com/rag/raggeneratorapplication/RagGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.rag.raggeneratorapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagGeneratorApplication.class, args);
+    }
+
+}
